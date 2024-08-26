47b286ee54c10f562fd12309c32319c457e05bd4
Amir Jamshidi <aabbccddee9898@gmail.com>
2024-08-26 21:05:37 +0330

    Create Final Edit

--- app/_configs/database.ts
+++ app/_configs/database.ts
@@ -3,9 +3,7 @@ import mongoose from 'mongoose'
 const MONGO_URL = process.env.MONGO_URL || 'mongodb://devamir2_amir:amir.h.j@localhost:27017/devamir2_source'
 
 const connectToDB = async () => {
-
     if (!MONGO_URL) return console.log('MONGO URL NOT VALID');
-
     try {
         if (mongoose.connection.readyState === 0) {
             await mongoose.connect(MONGO_URL);
